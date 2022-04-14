f5c486d6f963db5b13cc4eb4617045684ff9a7a0
Jeannie Fu <jeanniefu@google.com>
2022-04-14 15:16:01 -0700

    Adding back our original changes from round 1

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -1,8 +1,43 @@
 #! /usr/bin/env python3
+import copy
 
 class GameOfLife():
-    def run(self):
-        print("I don't do much, yet.")
+    def run(self, board_input):
+        # Sample: "-3,-5 -4,-5"
+        # Board is dict(x, list of ys)
+        # Builds board from input
+        board = {}
+        for raw_cord in board_input.split(' '):
+            raw_x, raw_y = raw_cord.split(',')
+            x, y = int(raw_x), int(raw_y)
+            if x not in board:
+                board[x] = []
+            board[x].append(y)
+
+        # Apply rules to board
+        next_board = copy.deepcopy(board)
+        for x in board.keys():
+            for y in board[x]:
+                num_live_neighbors = self.countNumLiveNeighbors(board, x, y)
+                if num_live_neighbors < 2:
+                    # Remove (x,y) from next_board
+                    next_board[x].remove(y)
+                    if not next_board[x]:
+                        next_board.pop(x)
+
+        # Print next_board
+        output_str = ""
+        for x in board.keys():
+            for y in board[x]:
+                output_str += x + "," + y + " "
+
+
+    def countNumLiveNeighbors(self, board, x, y):
+        # Count up the number of live neighbors for given (x,y)
+                count = 0
+
+                fo[-1,0,1]
+        return count
 
 if __name__ == "__main__":
     GameOfLife().run()
--- test/test_game_of_life.py
+++ test/test_game_of_life.py
@@ -6,14 +6,17 @@ from src.game_of_life import GameOfLife
 
 class GameOfLifeTest(unittest.TestCase):
 
-    def test_run_doesnt_do_much(self):
+    def test_die_by_underpopulation(self):
+        # Test rule 1. Any live cell with fewer than two live neighbours dies,
+        # as if by underpopulation.
         game = GameOfLife()
         fake_stdout = io.StringIO()
 
         with contextlib.redirect_stdout(fake_stdout):
-            game.run()
+            game.run("-3,-5 -4,-5 -5,-5")
 
         output = fake_stdout.getvalue()
         fake_stdout.close()
 
-        self.assertEqual(output, "I don't do much, yet.\n")
+        # Should all die because they each only have 1 neighbor.
+        self.assertEqual(output, "-4,-5")
